03539b1d284ba04f3fc95f07d72abbcc90bdb258
Carlos Cavero <carlos.cavero@atos.net>
2019-04-15 23:18:00 +0200

    Add Swagger dependencies and configuration to Spring boot
    Close #3.

--- todo-backend/src/main/java/com/example/todo/TodoApplication.java
+++ todo-backend/src/main/java/com/example/todo/TodoApplication.java
@@ -24,9 +24,21 @@
  */
 package com.example.todo;
 
+import java.util.ArrayList;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class TodoApplication extends SpringBootServletInitializer {
@@ -34,5 +46,28 @@ public class TodoApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
 	}
+	
+    @Configuration
+    @EnableSwagger2
+    public class SwaggerConfig {
+        @Bean
+        public Docket api() {
+            return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+                    .paths(PathSelectors.any()).build().apiInfo(metaData());
+        }
+    }
+
+    private ApiInfo metaData() {
+        ApiInfo apiInfo = new ApiInfo("Spring Boot REST API for TODO List", 
+                "TODO List", 
+                "See version", 
+                "-",
+                new Contact("Carlos Cavero", "", "cavero.carlos@gmail.com"), 
+                "Apache License, Version 2.0", 
+                "http://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>());
+        return apiInfo;
+    }
+
 
 }
